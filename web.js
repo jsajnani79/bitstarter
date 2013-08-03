d203ad42e90bf20c8d8d7efe06356f187326eba3
Ubuntu <ubuntu@ip-172-31-30-30.us-west-2.compute.internal>
2013-08-03 06:05:54 +0000

    Added buffer to web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
+var buffer = fs.readFileSync(index.html);
 app.get('/', function(request, response) {
-  response.send('Hi Neil!');
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
